0864b2eb27b3353fb06e208f98e796cafef02340
PierreBezuchow <axel@starosta.pl>
2018-05-22 13:26:53 +0200

    Added get, post, put, delete requests

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # easyhttp
+
+### Custom HTTP Library - Ajax with callback, created for training
+
+### Types of requests tested: 
+- GET
+- POST
+- PUT
+- Delete
---
+++ app.js
@@ -0,0 +1,55 @@
+const http = new easyHTTP;
+
+// Get Posts
+// http.get('https://jsonplaceholder.typicode.com/posts', function(err, posts){
+//   if(err) {
+//     console.log(err);
+//   }else {
+//     console.log(posts);
+//   }
+  
+// });
+
+// Get single post
+// http.get('https://jsonplaceholder.typicode.com/posts/3', function(err, post){
+//   if(err) {
+//     console.log(err);
+//   }else {
+//     console.log(post);
+//   }
+  
+// });
+
+// Create data
+const data = {
+  title: 'Custom post',
+  body: 'This is a custom post created for fun'
+};
+
+// Create post
+// http.post('https://jsonplaceholder.typicode.com/posts', data, function(err, post) {
+//   if(err) {
+//         console.log(err);
+//       }else {
+//         console.log(post);
+//       }
+// });
+
+// // Update post
+// http.put('https://jsonplaceholder.typicode.com/posts/23', data, function(err, post) {
+//   if(err) {
+//     console.log(err);
+//   } else {
+//     console.log(post);
+//   }
+// });
+
+// Delete post
+http.delete('https://jsonplaceholder.typicode.com/posts/1', function(err, response){
+  if(err) {
+    console.log(err);
+  }else {
+    console.log(response);
+  }
+  
+});
